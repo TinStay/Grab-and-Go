ae7641976302a743605b25c838b56439d11e9b7b
Martin <marti.staykov@gmail.com>
2021-03-14 12:38:21 +0100

    Move logo styles scss code

--- grab-and-go/components/Logo.js
+++ grab-and-go/components/Logo.js
@@ -1,11 +1,12 @@
 import Logo from "../public/images/Grab&GoLogoV4.svg";
 import Link from "next/link";
+import classes from "../styles/Navbar.module.scss";
 
 export default () => (
-  <div className="logo-container">
+  <div className={classes.logo_container}>
     <Link href="/">
       <Logo
-        className="logo"
+       className={classes.logo}
         viewBox="0 80 500 100"
         preserveAspectRatio="xMaxYMax meet"
       />
