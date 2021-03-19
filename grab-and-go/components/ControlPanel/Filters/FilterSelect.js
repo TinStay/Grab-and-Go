0723cf07af7f410f718eff325d80a639b9bcc664
Martin <marti.staykov@gmail.com>
2021-03-19 12:46:46 +0100

    Add filter option values

--- grab-and-go/components/ControlPanel/Filters/FilterSelect.js
+++ grab-and-go/components/ControlPanel/Filters/FilterSelect.js
@@ -1,51 +1,62 @@
-import React,{ useState } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import InputLabel from '@material-ui/core/InputLabel';
-import FormHelperText from '@material-ui/core/FormHelperText';
-import FormControl from '@material-ui/core/FormControl';
-import Select from '@material-ui/core/Select';
-import NativeSelect from '@material-ui/core/NativeSelect';
-import classes from '../../../styles/Home.module.scss'
+import React, { useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import InputLabel from "@material-ui/core/InputLabel";
+import FormControl from "@material-ui/core/FormControl";
+import Select from "@material-ui/core/Select";
+import classes from "../../../styles/Home.module.scss";
+import * as filterOptions from "./FilterOptions";
 
-const FilterSelect = (props) => {{}
+const FilterSelect = (props) => {
+  {
+  }
   const [selectedValue, setSelectedValue] = useState(null);
 
-  // const handleChange = (event) => {
-  //   const name = event.target.name;
-  //   setState({
-  //     ...state,
-  //     [name]: event.target.value,
-  //   });
-  // };
-  
-    return (
-      <FormControl className="w-100 mb-2">
-        <InputLabel htmlFor={`${props.name}-select`}>{props.label}</InputLabel>
-        <Select
-          native
-          value={selectedValue}
-          onChange={e => props.handleFilterChange(e)}
-          inputProps={{
-            name: props.name,
-            id: `${props.name}-select`
-          }}
-        >
-          <option aria-label="None" value="" />
-          <option value={10}>Ten</option>
-          <option value={20}>Twenty</option>
-          <option value={30}>Thirty</option>
-        </Select>
-      </FormControl>
-    )
-}
+  let options = [];
 
-export default FilterSelect
+  switch (props.name) {
+    case "storeType":
+      options = filterOptions.storeType;
+      break;
+    case "sortBy":
+      options = filterOptions.sortBy;
+      break;
+    case "range":
+      options = filterOptions.range;
+      break;
+    case "location":
+      options = filterOptions.location;
+      break;
+      dafault: break;
+  }
 
-        // <div className="col-6 my-2">
-        //   <p className="mb-2">{props.label}</p>
-        //   <select className="w-100">
-        //     <option value="1">1</option>
-        //     <option value="2">2</option>
-        //     <option value="3">3</option>
-        //   </select>
-        // </div>
+  return (
+    <FormControl className="w-100 mb-2">
+      <InputLabel htmlFor={`${props.name}-select`}>{props.label}</InputLabel>
+      <Select
+        native
+        value={selectedValue}
+        onChange={(e) => props.handleFilterChange(e)}
+        inputProps={{
+          name: props.name,
+          id: `${props.name}-select`,
+        }}
+      >
+       {options && options.map((option) => {
+         return <option value={option}>{option}</option>
+       })}
+        
+      </Select>
+    </FormControl>
+  );
+};
+
+export default FilterSelect;
+
+// <div className="col-6 my-2">
+//   <p className="mb-2">{props.label}</p>
+//   <select className="w-100">
+//     <option value="1">1</option>
+//     <option value="2">2</option>
+//     <option value="3">3</option>
+//   </select>
+// </div>
