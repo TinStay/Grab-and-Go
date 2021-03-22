3d0f3c23d336486cec0a2798b5b29655d423cb87
Martin <marti.staykov@gmail.com>
2021-03-22 14:56:22 +0100

    Remove colored text field style

--- grab-and-go/components/AuthForms/ColoredTextField.js
+++ grab-and-go/components/AuthForms/ColoredTextField.js
@@ -1,38 +1,38 @@
-import React from 'react'
-import classes from '../../styles/Auth.module.scss'
-import { withStyles } from '@material-ui/core/styles'
-import { TextField } from '@material-ui/core'
+import React from "react";
+import classes from "../../styles/Auth.module.scss";
+import { withStyles, makeStyles } from "@material-ui/core/styles";
+import { TextField } from "@material-ui/core";
 
-const ColoredTextField = withStyles({
-    root: {
-      "& label.Mui-focused": {
-        color: "#7eb92c",
-      },
-      "& .MuiInput-underline:after": {
-        borderBottomColor: "#7eb92c",
-      },
-      "& .MuiOutlinedInput-root": {
-        "&.Mui-focused fieldset": {
-          borderColor: "#7eb92c",
-        },
-      },
-    },
-  })(TextField);
-
+// const ColoredTextField = withStyles({
+//   root: {
+//     "& label.Mui-focused": {
+//       color: "#f50057"
+//     },
+//     "& .MuiInput-underline:after": {
+//       borderBottomColor: "#f50057"
+//     },
+//     "& .MuiOutlinedInput-root": {
+//       "&.Mui-focused fieldset": {
+//         borderColor: "#f50057"
+//       }
+//     }
+//   }
+// })(TextField);
 
 const ColoredTextInput = (props) => {
-    return (
-        <ColoredTextField
-              className={classes.form_field}
-              value={props.value}
-              onChange={(e) => props.handleChange(e)}
-              type={props.type}
-              id={`${props.name}-field`}
-              label={props.label}
-              name={props.name}
-              size="small"
-            />
-    )
-}
+  return (
+    <TextField
+      className={classes.form_field}
+      value={props.value}
+      onChange={(e) => props.handleChange(e)}
+      type={props.type}
+      id={`${props.name}-field`}
+      name={props.name}
+      label={props.label}
+      size="small"
+      fullWidth
+    />
+  );
+};
 
-export default ColoredTextInput
+export default ColoredTextInput;
